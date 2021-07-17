debfe57782cc3810bc0711102d2b47079cfd92da
Dav-14 <david.ragot14@gmail.com>
2021-07-17 19:20:01 +0200

    Ajout d'un formulaire

---
+++ src/pages/form.php
@@ -0,0 +1,10 @@
+<form method="post" action="newpage.php">
+ 
+<p>
+    On insèrera ici les éléments de notre formulaire.
+</p>
+<input type="text" name="pseudo" value="Dav-14"/>
+
+<input type="submit" value="Valid"/> 
+
+</form>
